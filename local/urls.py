8d836ed0db80a51dde8f9ae663a3be95ab92ae95
Lalisfeed <akhilraj1729@gmail.com>
2020-10-19 19:11:22 +0530

    menu+settings+history

--- local/urls.py
+++ local/urls.py
@@ -3,11 +3,13 @@ from . import views
 
 app_name = "local"
 urlpatterns = [
-    path('', views.auth, name="auth"),
-    path('settings/', views.settings, name="settings"),
-    path('order/', views.orders, name="orders"),
-    path('offline/', views.offline, name="offline"),
-    path('<str:error>', views.error, name="error"),
-    path('<str:error>/', views.error, name="error"),
+    path('', views.auth, name="auth"), # authentication page for all users
+    path('menu/', views.orders, name="orders"), # make orders looking at the menu
+    path('settings/', views.settings, name="settings"), # add or remove items in menu
+    path('orders/', views.history, name="history"), # list past orders
+
+    path('offline/', views.offline, name="offline"), # page to load when offline
+    path('<str:error>', views.error, name="error"), # incorrect path
+    path('<str:error>/', views.error, name="error"), # incorrect path
 ]
 
--- local/views.py
+++ local/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
+from django import forms
 from django.http import HttpResponse
 # Create your views here.
 
-# def index(request):
-#     return HttpResponse('Local')
+
 
 # page for authentication 
 def auth(request):
     return render(request, 'local/auth.html')
 
 
-# page for adding or deleting any items from the  owners database
-def settings(request):
-    return render(request, 'local/editpage.html')
-
-
-# page for all orders acceptance + calculate bills
+# page for menu + all orders acceptance + calculate bills
 def orders(request):
     return render(request, 'local/order.html')
+
+
+# page for adding or deleting any items from the  owners database
+def settings(request):
+    return render(request, 'local/settings.html')
+
+
+# page for listing past orders
+def history(request):
+    return render(request, 'local/history.html')
 
 
 # page to render when offline
