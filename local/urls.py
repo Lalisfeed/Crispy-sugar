eaad1c9bec6b13a7a6bb1aaa816a0b0fda6add90
Lalisfeed <akhilraj1729@gmail.com>
2020-11-02 10:45:07 +0530

    logout view corrected

--- local/urls.py
+++ local/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('orders/', views.orders, name="orders"),  # list past orders
     path('settings/', views.settings, name="settings"), # add or remove items in menu
     path('profile/', views.profile, name="profile"),  # list past orders
-    path('loggedout/', views.noauth, name="loggedout"),  # load the logout page
+    path('loggedout/', views.noauth, name="noauth"),  # load the logout page
 
     path('offline/', views.offline, name="offline"), # page to load when offline
     path('<str:error>', views.error, name="error"), # incorrect path
