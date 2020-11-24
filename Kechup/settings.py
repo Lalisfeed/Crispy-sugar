9a7f68c88d62e255d597962754ebeb7ddda57b31
Lalisfeed <akhilraj1729@gmail.com>
2020-11-24 17:11:17 +0530

    mysql comment

--- Kechup/settings.py
+++ Kechup/settings.py
@@ -79,6 +79,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': 'kechdb',
+        # 'USER': 'tommy',
+        # 'PASSWORD': 'TOM@xx7',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
     }
 }
 
